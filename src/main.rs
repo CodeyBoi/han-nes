d55a1b160d0a73ac03382917469a43ddb1676350
CodeyBoi <hannes.ryberg00@gmail.com>
2025-05-17 14:55:51 +0200

    Added roms and drawing logic

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use chip8::Chip8;
 mod chip8;
 
 fn main() {
-    let c8 = Chip8::new();
+    let mut c8 = Chip8::new();
+    c8.load_rom("chip8-roms/games/Pong (1 player).ch8").unwrap();
+    c8.run();
 }
